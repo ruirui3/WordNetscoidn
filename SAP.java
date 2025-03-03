a97fdff57af2cc1d34c107ab381d9868d2d3b9ba
ruirui3 <ruiruithing@gmail.com>
2025-03-02 22:52:07 -0800

    sap finish?

--- SAP.java
+++ SAP.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
@@ -5,13 +7,22 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class SAP {
 
+    private Digraph digraph;
+
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
-
+        digraph = new Digraph(G);
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
+
+        HashSet<Integer> hashV = new HashSet<Integer>();
+        hashV.add(v);
+        HashSet<Integer> hashW = new HashSet<Integer>();
+        hashV.add(w);
+
+        return length(hashV, hashW);
 
     }
 
@@ -19,17 +30,67 @@ public class SAP {
     // -1 if no such path
     public int ancestor(int v, int w) {
 
+        if (length(v, w) >= 1) {
+            return v;
+        }
+
+        return -1;
+
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in
     // w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
 
+        BreadthFirstDirectedPaths breadv = new BreadthFirstDirectedPaths(digraph, v);
+        BreadthFirstDirectedPaths breadw = new BreadthFirstDirectedPaths(digraph, w);
+        int maxLen = Integer.MAX_VALUE;
+
+        for (int i = 0; i < digraph.V(); i++) {
+
+            if (breadv.hasPathTo(i) && breadw.hasPathTo(i)) { // see if there is a path of ancestor i connected and
+                                                              // loops for number of V
+
+                int tempMax = breadv.distTo(i) + breadw.distTo(i);
+
+                maxLen = Math.min(tempMax, maxLen);
+            }
+
+        }
+
+        if (maxLen != Integer.MAX_VALUE) {
+            return maxLen;
+        }
+        return -1;
+
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such
     // path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+
+        BreadthFirstDirectedPaths breadv = new BreadthFirstDirectedPaths(digraph, v);
+        BreadthFirstDirectedPaths breadw = new BreadthFirstDirectedPaths(digraph, w);
+        int maxLen = Integer.MAX_VALUE;
+        int ancestor = -1;
+
+        for (int i = 0; i < digraph.V(); i++) {
+
+            if (breadv.hasPathTo(i) && breadw.hasPathTo(i)) { // see if there is a path of ancestor i connected and
+                                                              // loops for number of V
+
+                int tempMax = breadv.distTo(i) + breadw.distTo(i);
+
+                if (tempMax < maxLen) {
+                    maxLen = Math.min(tempMax, maxLen);
+                    ancestor = i;
+                }
+
+            }
+
+        }
+
+        return ancestor;
 
     }
 
