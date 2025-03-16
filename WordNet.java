43ae52849e9e01cc04677531538aec881416ca61
Ehehing <ruiruithing@gmail.com>
2025-03-15 23:15:39 -0700

    bbggg

--- WordNet.java
+++ WordNet.java
@@ -1,22 +1,59 @@
+import java.util.HashSet;
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class WordNet {
+
+    private HashSet<String> numOfNoun;
+    private Stack<Integer> numOfID;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
+        
+        In in = new In(synsets);
+
+        numOfNoun = new HashSet<String>();
+        numOfID = new Stack<Integer>();
+
+        while (in.hasNextLine()) {
+
+            String[] synset = in.readLine().split(",");
+            int id = Integer.parseInt(synset[0]);
+            
+            numOfID.push(id);
+            
+            String[] numOfNouns = synset[1].split(" ");
+
+            for (int i = 0; i<numOfNouns.length; i++) {
+                numOfNoun.add(numOfNouns[i]);
+            }
+
+        }
 
     }
 
     // returns all WordNet nouns
     public Iterable<String> nouns() {
+        
+        return numOfNoun;
 
     }
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
 
+        return numOfNoun.contains(word);
+
     }
 
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
+
+        
 
     }
 
