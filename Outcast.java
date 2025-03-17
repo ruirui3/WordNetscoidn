19e6543d14c9e5d44bec86811f02fe01b3a3fb66
Ehehing <ruiruithing@gmail.com>
2025-03-16 22:58:08 -0700

    88% (first try?)

--- Outcast.java
+++ Outcast.java
@@ -2,12 +2,38 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Outcast {
+
+    private WordNet wordnetRizz;
+
     public Outcast(WordNet wordnet) {
+        if (wordnet == null) {
+            throw new IllegalArgumentException();
+        }
+        wordnetRizz = wordnet; 
 
     } // constructor takes a WordNet object
 
     public String outcast(String[] nouns) {
-        
+        int mostUnrelated = Integer.MIN_VALUE;
+        String outcast = "";
+        for (String a : nouns) {
+
+            int distance = 0;
+
+            for (String b : nouns) {
+                distance += wordnetRizz.distance(a, b);
+            }
+
+            //same logic as sap
+            if (distance > mostUnrelated) {
+                mostUnrelated = distance;
+                outcast = a;
+            }
+
+        }
+
+        return outcast;
+
     } // given an array of WordNet nouns, return an outcast
 
     public static void main(String[] args) {
--- WordNet.java
+++ WordNet.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.DirectedCycle;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdIn;
@@ -11,8 +15,8 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class WordNet {
 
-    private List<String> numOfNoun;
-    private List<Integer> numOfID;
+    private Map<String, List<Integer>> nounToIDs;
+    private ArrayList<String> synsetList;
     private Digraph digraph;
     private SAP sap;
     
@@ -20,38 +24,60 @@ public class WordNet {
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
         
+        if (synsets == null || hypernyms == null) {
+            throw new IllegalArgumentException();
+        }
+
         In in = new In(synsets);
 
-        numOfNoun = new ArrayList<String>();
-        numOfID = new ArrayList<Integer>();
+        nounToIDs = new HashMap<>();
+        synsetList = new ArrayList<String>();
 
         while (in.hasNextLine()) {
 
             String[] synset = in.readLine().split(",");
-            
+            int id = Integer.parseInt(synset[0]);
             String[] numOfNouns = synset[1].split(" ");
 
+            synsetList.add(synset[1]);
 
-            for (int i = 0; i<numOfNouns.length; i++) {
-                numOfNoun.add(numOfNouns[i]);
+            for (String noun : numOfNouns) {
+                if (!nounToIDs.containsKey(noun)) {
+                    nounToIDs.put(noun, new ArrayList<>()); //marks entire noun structure into one hyp
+                }
+                nounToIDs.get(noun).add(id);
             }
 
         }
 
-        digraph = new Digraph(numOfNoun.size());
+        digraph = new Digraph(synsetList.size());
 
         In bingBingBongBong = new In(hypernyms);
         while (bingBingBongBong.hasNextLine()) {
 
             String[] bingBongList = bingBingBongBong.readLine().split(",");
-            numOfID.add(Integer.parseInt(bingBongList[0]));
+            int id = Integer.parseInt(bingBongList[0]);
             
             for (int i = 1; i < bingBongList.length; i++) {
                 
-                digraph.addEdge(Integer.parseInt(bingBongList[0]), Integer.parseInt(bingBongList[i]));
+                digraph.addEdge(id, Integer.parseInt(bingBongList[i]));
 
             }
 
+        }
+
+        DirectedCycle dc = new DirectedCycle(digraph);
+        if (dc.hasCycle()) {
+            throw new IllegalArgumentException();
+        }
+        int roots = 0;
+        for (int v = 0; v < digraph.V(); v++) {
+            if (digraph.outdegree(v) == 0) {
+                roots++;
+            }
+        }
+        if (roots > 1) {
+            throw new IllegalArgumentException();
         }
 
         sap = new SAP(digraph);
@@ -61,45 +87,62 @@ public class WordNet {
     // returns all WordNet nouns
     public Iterable<String> nouns() {
         
-        return numOfNoun;
+        return nounToIDs.keySet();
 
     }
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
-
-        return numOfNoun.contains(word);
+        if (word == null) {
+            throw new IllegalArgumentException();
+        }
+        return nounToIDs.containsKey(word);
 
     }
 
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
+        if (nounA == null || nounB == null) {
+            throw new IllegalArgumentException();
+        
+        }
+        if (!isNoun(nounA) || !isNoun(nounB)) {
+            throw new IllegalArgumentException();
+        }
 
         return sap.length(nounToHyp(nounA), nounToHyp(nounB));
 
     }
 
     private int nounToHyp(String noun) {
-        return numOfID.get(numOfNoun.indexOf(noun));
+        if (!nounToIDs.containsKey(noun)) {
+            throw new IllegalArgumentException();
+        }
+        return nounToIDs.get(noun).get(0);
     }
 
     private String hypToNoun(int hyp) {
-        return numOfNoun.get(numOfID.indexOf(hyp));
+        if (hyp<0 || hyp >= synsetList.size()) {
+            throw new IllegalArgumentException();
+        }
+        return synsetList.get(hyp);
     }
-
-
-
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA
     // and nounB
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
+
+        if (nounA == null || nounB == null || !isNoun(nounA) || !isNoun(nounB) ) {
+            throw new IllegalArgumentException();
+        }
+
         return hypToNoun(sap.ancestor(nounToHyp(nounA), nounToHyp(nounB)));
         
     }
 
     // do unit testing of this class
     public static void main(String[] args) {
-
-    }
+        
+      }
 }
